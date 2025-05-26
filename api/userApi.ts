0118822f5defbe1a7fea02b9314099cf56fdab22
tonhat211 <21130463@st.hcmuaf.edu.vn>
2025-05-26 14:20:13 +0700

    binh luan san pham

---
+++ api/userApi.ts
@@ -0,0 +1,16 @@
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// Lưu dữ liệu
+await AsyncStorage.setItem('userId', '1');
+
+// Đọc dữ liệu
+// const token = await AsyncStorage.getItem('userName');
+
+// Xóa dữ liệu
+// await AsyncStorage.removeItem('token');
+  
+
+// const userId = await AsyncStorage.getItem('userId');
+
+
